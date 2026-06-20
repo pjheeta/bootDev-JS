5101dff9b430bc792f6d4d40513ffc48e4fe4bd1
pjheeta <pjheeta@gmail.com>
2026-06-20 09:20:18 -0700

    All done!

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const NAME = `Pawan`;
+import { say } from "cowsay";
+
 import{name} from "./moo.js";
-console.log(`${name}, ${NAME}!`);
+const NAME = `Pawan`;
+
+//console.log(`${name}, ${NAME}!`);
+console.log(say({ text: `moo, ${NAME}!` }));
